99b90bd82758f5331aee243a96a984321644c921
Nguyen <68248147+Ne-k@users.noreply.github.com>
2024-02-23 13:51:25 -0800

    Finished pig

--- Nguyen_game_of_pig.py
+++ Nguyen_game_of_pig.py
@@ -1,28 +1,8 @@
 """
-The Game of Pig is a classic game played with a 6 sided die. In the game a player rolls the die. If they roll a 2
-through 6, they add that score to their round score, but if they roll a 1, their round is over and their round score
-resets to zero.
-
-At any point, the player can choose to bank their round score. When they do that, the points are added to their overall
-score and their round score resets back to zero.
-
-Once the player’s overall score reaches 100, they win. While the die roll is random, there is a little strategy when
-deciding whether to bank or continue in the round. If you try to be a pig and get more points, you may lose everything
-and have to start over!
-
-More information on the game can be found on wikipedia.
-
-For the first part of this project, you are going to make the basic gameplay and keep track of how many turns it takes
-to win. At the start of each turn, you should display the turn number and the player’s score. Each round, you should
-ask the player if they want to roll or bank the points. If they bank, you move on to the next turn, otherwise you roll
-again. If they roll a 1, then that round ends and the round score is reset. Otherwise, add the number from the die to
-the round score. Play continues like that until you get to 100 or more.
-
-You can implement this several different ways. One thing to consider is that in the next activity, you will be adding
-a computer player. Keep that in mind as you think about the design.
-
-You’ll also need to include randomization in your program to simulate rolling a die. There is information on including
-randomization in the docs tab (Basics > Random Numbers) or in this tutorial.
+Name: Cardin Nguyen
+Date: 2/24/24
+Assignment: Game of pig
+Description: Play a game of pig against a computer, first one to 100 wins.
 """
 
 import random
