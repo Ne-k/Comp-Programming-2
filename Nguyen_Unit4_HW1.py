9f73745f9610f3f84d8ba89febb8e08f373971d5
npspy <username.lilrice@gmail.com>
2024-02-25 12:49:59 -0800

    Brain rot

--- Nguyen_Unit4_HW1.py
+++ Nguyen_Unit4_HW1.py
@@ -5,12 +5,15 @@ Assignment: Unit 4 HW 1
 Description:
 Problem 1
 
-Create a function called list_of_cubes(start, stop). That function should make, and return, a list of the first 10 cubes using list comprehension. Be sure to use the parameters when you call it e.g. list_of_cubes(5,10). Then print the values from each of those lists to the console
+Create a function called list_of_cubes(start, stop). That function should make, and return, a list of the first 10 cubes
+using list comprehension. Be sure to use the parameters when you call it e.g. list_of_cubes(5,10). Then print the values
+from each of those lists to the console
 
 
 Problem 2
 
-Create a function called pizza_modifier(topping_list, new_topping) that allows a user to add a new topping to a list of toppings. The function should print the current toppings and print the toppings with the new topping added.
+Create a function called pizza_modifier(topping_list, new_topping) that allows a user to add a new topping to a list of
+toppings. The function should print the current toppings and print the toppings with the new topping added.
 
 """
 
@@ -34,11 +37,24 @@ def list_of_cubes(start: int, stop: int) -> None:
 
 # Problem 2
 def pizza_modifier(toppings: list, new_topping: str) -> None:
-    return None
-
+    """
+    Allows the user to add a new topping to a list of toppings
+    :param toppings: Current List
+    :param new_topping: New toppings to add to toppings
+    :return: Nothing, it prints to the console the new list
+    """
+    try:
+        if not isinstance(toppings, list) or not isinstance(new_topping, str):
+            raise ValueError
+        print(f"Current toppings: {toppings}")
+        toppings.append(new_topping)
+        print(f"New toppings: {toppings}")
+    except ValueError:
+        print("Invalid input: Toppings have to be a list and new_topping has to be a string.")
 
 def main():
     list_of_cubes(5, 10)
+    pizza_modifier(["pepperoni", "cheese", "mushrooms"], "onions")
 
 
 if __name__ == "__main__":
