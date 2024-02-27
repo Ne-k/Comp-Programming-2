bfedf9e1da6ada901b17201f7f05ae072e019913
Nguyen <68248147+Ne-k@users.noreply.github.com>
2024-02-27 14:44:03 -0800

    aaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
 '''
-Name: Cardin Nguyen
-Date: 2/27/24
-Topic: Unit 4 - lists, tuples, list methods
+Name: Mr. Smith
+Date: 2/26/24
+Lesson: Unit 5
+Topic: 2D Lists
 '''
 
+# 2D lists are like a grid
+#           0  1 2  3 4 5 6 7
+my_grid = [[-3,2,0,-1,7,8,3,9], # row 0
+            [5,9,-3,-5,3,9,0,-9], # row 1
+            [2,0,3,3,7,8,3,2],
+            [9,-3,7,7,3,9,0,9],
+            [3,3,7,8,3,2,7,8],
+            [7,3,3,3,7,8,3,9],
+            [2,7,7,3,3,-5,7,8],
+            [9,-3,7,7,7,-5,3,9]
+]
 
-my_list = ["taco","salad","pot roast","cake"]
-my_list[3] = "ice cream" # this is valid
-my_tuple = ("taco","salad","pot roast","cake")
-print(my_tuple)
-# my_tuple[3] = "ice cream" # not valid
+# print the first row
+print(my_grid[0])
 
-#                   0      1     2       3   list indices
-#                  0 1    0 1   0 1    0   1 tuple indices
-list_of_points = [(1,2),(-4,5),(7,4),(12,-12)]
-print(list_of_points[2][0]) # prints 7
-print(list_of_points[2:]) # prints [(7,4),(12,-12)]
-print(list_of_points[1]) # (-4,5)
-# change (7,4) to (-7,4)
-# list_of_points[2][0] = -7 is not valid
-list_of_points[2] = (-7,4)
-print(list_of_points)
+#print the last row
+print(my_grid[7])
+print(my_grid[-1])
+print(my_grid[len(my_grid)-1])
+# len(my_grid) returns the number of rows
 
-tuple_of_lists = ([1,2],[3,4],[5,6])
-print(tuple_of_lists[0][1]) # 2
-tuple_of_lists[2][0] = -5 #valid
-# tuple_of_lists[2] = [-5,6] #invalid
-big_tuple = ([1,2,3,4,5],[6,7,8])
-# print 3,4,5
-print(big_tuple[0][2:])
+# print [7,7,7] from the last row
+# Steps: access last row, access slice of 7,7,7
+print(my_grid[-1][2:5])
+
+# print 2nd through 5th rows
+print(my_grid[1:5])
+for i in range(1,5):
+    print(my_grid[i])
+
+# print all of the data from the 3rd column
+# should print [0,-3,3,7,7,3,7,7]
+
+target = [0,-3,3,7,7,3,7,7]
+column_three = []
+
+# fill up column_three
+for i in range(len(my_grid)):
+    column_three.append(my_grid[i][2])
+
+assert target == column_three
+
+# pythonic way
+column_three = []
+for index,item in enumerate(my_grid):
+    column_three.append(my_grid[index][2])
+
+assert target == column_three
+
+my_grid.append([100,100,100,100])
+
+for item in my_grid:
+    print(item)
