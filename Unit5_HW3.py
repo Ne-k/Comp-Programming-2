8e45f307f9b22b2691f8bcfcc99f0a327c5ce5f1
Cardin <nekk.ng3@gmail.com>
2024-03-14 09:17:32 -0700

    Finished hw

--- Unit5_HW3.py
+++ Unit5_HW3.py
@@ -4,6 +4,7 @@ Unit 5 HW 3
 Description:  https://docs.google.com/document/d/1_TKrhQSEWTh4AweB_w2UN1hikE7vz4O_Us9st90PVCk/edit
 """
 
+
 # Problem 1
 def reverse_lookup(d: dict, v: str) -> list:
     """
@@ -20,81 +21,103 @@ def reverse_lookup(d: dict, v: str) -> list:
 
 
 
-"""On some cell phones, text messages can be sent using the numeric keypad. Because each key has multiple letters 
-associated with it, multiple key presses are needed for most letters. Pressing the number once generates the first 
-letter on the key. Pressing the number 2, 3, 4 or 5 times generates the second, third, fourth or fifth character 
-listed for that key.
-
-Write a program that displays the key presses that must be made to enter a text message read from the user. Construct 
-a dictionary that maps from each letter or symbol to the key presses. Then use the dictionary to generate and display 
-the presses for the user’s message. For example, if the user enters Hello, World! then your program should output 
-4433555555666110966677755531111. Ensure that your program handles both uppercase and lowercase letters. Ignore any 
-characters that aren’t listed in the table above such as semicolons and brackets.
-
-"""
 
 # Problem 2
-def text_to_keypress(text: str) -> str:
-    """
-    This function takes a string and returns the keypresses needed to enter the string on a cell phone.
-    :param text: string
-    :return: string
-    """
-    text = text.lower()
-    keypresses = ""
-    keymap = {
-        "a": "2",
-        "b": "22",
-        "c": "222",
-        "d": "3",
-        "e": "33",
-        "f": "333",
-        "g": "4",
-        "h": "44",
-        "i": "444",
-        "j": "5",
-        "k": "55",
-        "l": "555",
-        "m": "6",
-        "n": "66",
-        "o": "666",
-        "p": "7",
-        "q": "77",
-        "r": "777",
-        "s": "7777",
-        "t": "8",
-        "u": "88",
-        "v": "888",
-        "w": "9",
-        "x": "99",
-        "y": "999",
-        "z": "9999",
-        " ": "0",
+def text_to_keypress(text):
+    # Define the mapping of characters to key presses
+    keypresses = {
+        'a': '2', 'b': '22', 'c': '222',
+        'd': '3', 'e': '33', 'f': '333',
+        'g': '4', 'h': '44', 'i': '444',
+        'j': '5', 'k': '55', 'l': '555',
+        'm': '6', 'n': '66', 'o': '666',
+        'p': '7', 'q': '77', 'r': '777', 's': '7777',
+        't': '8', 'u': '88', 'v': '888',
+        'w': '9', 'x': '99', 'y': '999', 'z': '9999',
+        ' ': '0', ',': '11', '.': '33', '!': '1111', '?': '9999'
     }
+    text = text.lower()
+    result = ""
     for char in text:
-        if char in keymap:
-            keypresses += keymap[char]
-    return keypresses
+        if char in keypresses:
+            result += keypresses[char]
+
+    return result
+
+# Problem 3
+def count_unique_chars():
+    text = input("Enter a string: ")
+    unique_chars = {}
+    for char in text:
+        if char not in unique_chars:
+            unique_chars[char] = 1
+    print(f"The string has {len(unique_chars)} unique characters.")
+
+# Problem 4
+def number_to_words(n):
+    ones = {
+        1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+        6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'
+    }
+    teens = {
+        10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+        15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+    }
+    tens = {
+        2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty',
+        6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'
+    }
+
+    if 1 <= n < 10:
+        return ones[n]
+    elif 10 <= n < 20:
+        return teens[n]
+    elif 20 <= n < 100:
+        if n % 10 == 0:
+            return tens[n // 10]
+        else:
+            return tens[n // 10] + ' ' + ones[n % 10]
+    elif 100 <= n < 1000:
+        if n % 100 == 0:
+            return ones[n // 100] + ' hundred'
+        else:
+            return ones[n // 100] + ' hundred ' + number_to_words(n % 100)
+
+
+# Test the function
+
 def main():
+    print("\033[94mCardin Nguyen\033[0m")
+
     # Problem 1
-    # frEn = {
-    #     "le": "the",
-    #     "la": "the",
-    #     "livre": "book",
-    #     "pomme": "apple",
-    # }
-    # print("The french words for 'the' are: ", reverse_lookup(frEn, "the"))
-    # print("Expected: ['le', 'la']")
-    # print()
-    # print("The french words for 'apple' are: ", reverse_lookup(frEn, "apple"))
-    # print("Expected: ['pomme']")
-    # print()
-    # print("The french words for 'asdf' are: ", reverse_lookup(frEn, "asdf"))
-    # print("Expected: []")
+    frEn = {
+        "le": "the",
+        "la": "the",
+        "livre": "book",
+        "pomme": "apple",
+    }
+    print("The french words for 'the' are: ", reverse_lookup(frEn, "the"))
+    print("Expected: ['le', 'la']")
+    print()
+    print("The french words for 'apple' are: ", reverse_lookup(frEn, "apple"))
+    print("Expected: ['pomme']")
+    print()
+    print("The french words for 'asdf' are: ", reverse_lookup(frEn, "asdf"))
+    print("Expected: []")
+    print()
 
     # Problem 2
     print(text_to_keypress("Hello, World!"))
 
+    print()
+
+    # Problem 3
+    count_unique_chars()
+
+    print()
+
+    # Problem 4
+    print(number_to_words(int(input("Enter a number between 0 and 999: "))))
 
 if __name__ == "__main__":
     main()
